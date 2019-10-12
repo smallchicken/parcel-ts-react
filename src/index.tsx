99757dcfc62cac39739ccdba5a286033365e0e74
徐及 <xuji@ccbtrust.com.cn>
2019-10-12 11:37:37 +0800

    添加

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import 'babel-polyfill';
 import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd';
+
 
 const rootElement = document.getElementById('root')
 
 render(
-  <App />,
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>,
   rootElement
 )
